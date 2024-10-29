22e1c642ac60cb8604f5ed3371e8efdffa2f6df3
Ayo <talktojmcvibes@gmail.com>
2024-10-29 10:21:29 +0100

    debug getApps controller

--- controllers/getAppsController.js
+++ controllers/getAppsController.js
@@ -2,15 +2,17 @@ const userModel = require("../models/model");
 
 const getApps = async (req, res)=>{
     const user = req.user;
-   
+    //console.log(user)
 
     try {
         const userEmail = user.email
-        const userData = await userModel.findOne({userEmail});
+        //console.log({"user email": userEmail})
+        const userData = await userModel.findOne({email: userEmail});
+        console.log({"user data": userData})
         if (!userData){
             return res.status(404).json({message: "User not found"})
         }else {
-            return res.status(200).json(userData.apps)
+            return res.status(200).json(userData.apps || []);
         }
     } catch (err) {
         console.log('Error getting user data')
